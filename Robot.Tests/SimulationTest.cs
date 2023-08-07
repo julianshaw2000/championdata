d2aec12aac7b204623e8765fedf17b8f0cc2087b
julianshaw2000 <julianshaw2000@gmail.com>
2023-08-07 03:07:29 +0100

    More comments Unit tests

--- Robot.Tests/SimulationTest.cs
+++ Robot.Tests/SimulationTest.cs
@@ -6,19 +6,19 @@ namespace Robot.Tests;
 public class SimulationTest
 { 
         [Theory]
-        [InlineData("PLACE 0,0,NORTH\nMOVE\nREPORT", "0,1,NORTH")]  // Robot should move up
-        [InlineData("PLACE 0,0,NORTH\nLEFT\nREPORT", "0,0,WEST")]  // Robot should turn left
-        [InlineData("PLACE 2,2,SOUTH\nMOVE\nMOVE\nREPORT", "2,0,SOUTH")]  // Robot should move down twice
+        [InlineData("PLACE 0,0,NORTH\nMOVE\nREPORT", "0,1,NORTH")]  // Robot should move north
+        [InlineData("PLACE 0,0,NORTH\nLEFT\nREPORT", "0,0,WEST")]  // Robot should turn west
+        [InlineData("PLACE 2,2,SOUTH\nMOVE\nMOVE\nREPORT", "2,0,SOUTH")]  // Robot should move south twice
         [InlineData("PLACE 2,2,SOUTH\nMOVE\nMOVE\nRIGHT\nREPORT", "2,0,WEST")] // Robot should move and turn right
-        [InlineData("PLACE 2,2,SOUTH\nMOVE\nMOVE\nRIGHT\nMOVE\nREPORT", "1,0,WEST")]
+        [InlineData("PLACE 2,2,SOUTH\nMOVE\nMOVE\nRIGHT\nMOVE\nREPORT", "1,0,WEST")] // Robot should move south 2, turn west, and move 1
         [InlineData("MOVE\nREPORT", Constants.UnplacedRobot)]  
-        [InlineData("PLACE 0,0,NORTH\nLEFT\nMOVE\nREPORT", "0,0,WEST")]
-        [InlineData("PLACE 0,0,NORTH\nRIGHT\nMOVE\nREPORT", "1,0,EAST")]  
+        [InlineData("PLACE 0,0,NORTH\nLEFT\nMOVE\nREPORT", "0,0,WEST")]  // Robot should turn left
+        [InlineData("PLACE 0,0,NORTH\nRIGHT\nMOVE\nREPORT", "1,0,EAST")] // Robot should turn right 
         [InlineData("PLACE 0,0,SOUTH\nLEFT\nMOVE\nREPORT", "1,0,EAST")] // Robot should  move
         [InlineData("PLACE 0,0,SOUTH\nRIGHT\nMOVE\nREPORT", "0,0,WEST")] // Robot should not move
-        [InlineData("PLACE 0,0,NORTH\nRIGHT\nREPORT", "0,0,EAST")]  // Robot should not turn right
-        [InlineData("PLACE 4,4,NORTH\nRIGHT\nREPORT", "4,4,EAST")] // Robot should not turn right
-        [InlineData("PLACE 5,5,NORTH\nRIGHT\nREPORT", "5,5,EAST")] // Robot should not turn right
+        [InlineData("PLACE 0,0,NORTH\nRIGHT\nREPORT", "0,0,EAST")]  // Robot should turn right
+        [InlineData("PLACE 4,4,NORTH\nRIGHT\nREPORT", "4,4,EAST")] // Robot should turn right
+        [InlineData("PLACE 5,5,NORTH\nRIGHT\nREPORT", "5,5,EAST")] // Robot should turn right
         [InlineData("PLACE 5,5,NORTH\nMOVE\nREPORT", "5,5,NORTH")]   // Robot should not move
         [InlineData("PLACE 5,6,NORTH\nRIGHT\nREPORT", Constants.UnplacedRobot)] 
         public void TestToyRobotSimulation(string inputCommands, string expectedOutput)
